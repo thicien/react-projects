4be8588d3df9a39ca06f6f5657191b47b507f2b4
mugisha thicien <mugishathicien04@gmail.com>
2026-03-17 18:12:08 +0200

    Working on the form validation

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,21 @@
-import { useState } from "react"
-import type { Task } from "./types/Task"
-
 function App() {
-
-  const [tasks, setTasks] = useState<Task[]>([])
-  const [taskText, setTaskText] = useState<string>("")
-
-  const addTask = () => {
-    if (!taskText.trim()) return
-
-    const newTask: Task = {
-      id: Date.now(),
-      text: taskText,
-      completed: false
-    }
-    setTasks([...tasks, newTask])
-    setTaskText("")
-  }
-
-  const toggleTask = (id: number) => {
-    setTasks(
-      tasks.map(task =>
-        task.id === id
-          ? { ...task, completed: !task.completed }
-          : task
-      )
-    )
-  }
-
-  const deleteTask = (id: number) => {
-    setTasks(tasks.filter(task => task.id !== id))
-  }
-
   return (
-    <div>
-      <h1>Task Manager</h1>
-
-      <input
-        type="text"
-        value={taskText}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setTaskText(e.target.value)
-        }
-      />
-
-      <button onClick={addTask}>Add Task</button>
-
-      <ul>
-        {tasks.map(task => (
-          <li key={task.id}>
-            <span
-              onClick={() => toggleTask(task.id)}
-              style={{
-                textDecoration: task.completed ? "line-through" : "none",
-                cursor: "pointer"
-              }}
-            >
-              {task.text}
-            </span>
-
-            <button onClick={() => deleteTask(task.id)}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-
-      <p>Remaining Tasks: {tasks.filter(t => !t.completed).length}</p>
-
-    </div>
+    <section>
+      <h1>Signup form</h1>
+      <form>
+        <label htmlFor="email">Email:</label>
+        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
+        <br />
+        
+        <label htmlFor="password">Password:</label>
+        <input id="password" type="password" name="password" />
+        <br />
+        
+        <button></button>
+        
+      </form>
+    </section>
   )
 }
 
-export default App
+export default App;
