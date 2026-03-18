ebbc485107c4d006da0d60d9db104b792adf5905
mugisha thicien <mugishathicien04@gmail.com>
2026-03-18 16:49:51 +0200

    Working on form submission especially on radio buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,53 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
 function App() {
+
+
+  function signUp(formData) {
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const employmentStatus = formData.get("employmentStatus")
+    console.log(employmentStatus)
+  }
+
   return (
     <section>
       <h1>Signup form</h1>
-      <form>
+      <form action={signUp}>
+
         <label htmlFor="email">Email:</label>
-        <input id="email" type="email" name="email" placeholder="joe@schmoe.com" />
-        <br />
-        
+        <input id="email" defaultValue="joe@schmoe.com" type="email" name="email" placeholder="joe@schmoe.com" />
+
         <label htmlFor="password">Password:</label>
-        <input id="password" type="password" name="password" />
-        <br />
-        
-        <button type="submit">Submit</button>
-        
+        <input id="password" defaultValue="password123" type="password" name="password" />
+
+        <label htmlFor="description">Description:</label>
+        <textarea id="description" name="description" defaultValue="This is a description"></textarea>
+
+        <fieldset>
+          <legend>Employment Status:</legend>
+          <label>
+            <input type="radio" name="employmentStatus" value="unemployed" />
+            Unemployed
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="part-time" />
+            Part-time
+        </label>
+          <label>
+            <input type="radio" name="employmentStatus" value="full-time" />
+            Full-time
+        </label>
+        </fieldset>
+
+
+
+        <button>Submit</button>
+
       </form>
     </section>
   )
 }
 
-export default App;
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
