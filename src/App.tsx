7eb97d3ac24b55cb640ae903283ff952a3df0fb0
mugisha thicien <mugishathicien04@gmail.com>
2026-04-05 09:54:14 +0200

    Solving the layouts issue

--- src/App.tsx
+++ src/App.tsx
@@ -26,30 +26,16 @@ function App() {
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col items-center py-10'>
       <h1 className='text-6xl sm: text-8xl font-black text-gray-200 opacity-50 mb-8 tracking-widset lowercase'> todos</h1>
-      <div className='w-full max-w-2xl px-4'></div>
-    </div>
-  )
-
-}
-
-
-
-
-
-
-function App() {
-  return (
-    <div className="min-h-screen bg-gray-50 flex flex-col items-center py-10">
-      <h1 className="text-6xl sm:text-8xl font-black text-gray-200 opacity-50 mb-8 tracking-widest lowercase">todos</h1>
-      <div className="w-full max-w-2xl px-4">
-        <div className="bg-white shadow-lg rounded-full overflow-hidden mb-6 flex items-center px-6">
+      <div className='w-full max-w-2xl px-4'>
+        <div className='bg-white shadow-lg rounded-full overflow-hidden mb-6 flex items-center px-6'>
           <TaskForm onAdd={addTodo} />
         </div>
-        <div className="bg-transparent flex flex-col gap-4">
+        <div className='bg-transparent flex flex-col gap-4'>
           <TaskList todos={todos} onToggle={toggleTodo} onDelete={deleteTodo} />
         </div>
       </div>
     </div>
   );
+
 }
 export default App;
