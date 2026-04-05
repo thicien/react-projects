ec9c0ddedda55ed3ef1ae7320759878db0cf335b
mugisha thicien <mugishathicien04@gmail.com>
2026-04-05 09:41:07 +0200

    Adding delete button for different tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,43 @@
 import { useState } from 'react';
-import TaskList from './components/TaskList';
+import Tasklist from './components/TaskList';
 import TaskForm from './components/TaskForm';
 import type { Todo } from './types/index';
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: '1', text: 'IAmHere', completed: false },
+    { id: '1', text: 'I Am here', completed: false },
     { id: '2', text: 'reactjsexample.com', completed: false },
-    { id: '3', text: 'A To do app developed using to consolidate your knowledge about React', completed: false },
+    { id: '3', text: 'A To do app developed using to consolidate your knowledge about React', completed: false }
   ]);
 
   const addTodo = (text: string) => {
-    if (!text.trim()) return;
-    const newTodo: Todo = { id: crypto.randomUUID(), text, completed: false };
-    setTodos([newTodo, ...todos]);
-  };
-
+  if (!text.trim()) return;
+  const newTodo: Todo = { id: crypto.randomUUID(), text, completed: false };
+  setTodos([newTodo, ...todos]);
+  }; 
   const toggleTodo = (id: string) => {
     setTodos(todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t));
   };
 
   const deleteTodo = (id: string) => {
-    setTodos(todos.filter((t) => t.id !== id));
-  };
+   setTodos(todos.filter((t) => t.id !== id))
+  }
 
+  return (
+    <div className='min-h-screen bg-gray-50 flex flex-col items-center py-10'>
+      <h1 className='text-6xl sm: text-8xl font-black text-gray-200 opacity-50 mb-8 tracking-widset lowercase'> todos</h1>
+      <div className='w-full max-w-2xl px-4'></div>
+    </div>
+  )
+
+}
+
+
+
+
+
+
+function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center py-10">
       <h1 className="text-6xl sm:text-8xl font-black text-gray-200 opacity-50 mb-8 tracking-widest lowercase">todos</h1>
