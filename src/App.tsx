8fb3c46e004f80bd7cf874ff35fe374549a09ada
mugisha thicien <mugishathicien04@gmail.com>
2026-04-07 17:31:04 +0200

    Fixing the unimported component from TaskList.tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,26 @@
 import { useState } from 'react';
-import Tasklist from './components/TaskList';
 import TaskForm from './components/TaskForm';
+import TaskList from './components/TaskList';
 import type { Todo } from './types/index';
 
 function App() {
   const [todos, setTodos] = useState<Todo[]>([
-    { id: '1', text: 'I Am here', completed: false },
-    { id: '2', text: 'reactjsexample.com', completed: false },
+    { id: '1', text: 'I am A student', completed: false },
+    { id: '2', text: 'So far so good', completed: false },
     { id: '3', text: 'A To do app developed using to consolidate your knowledge about React', completed: false }
-  ]);
-
+  ])
   const addTodo = (text: string) => {
-  if (!text.trim()) return;
-  const newTodo: Todo = { id: crypto.randomUUID(), text, completed: false };
-  setTodos([newTodo, ...todos]);
-  }; 
-  const toggleTodo = (id: string) => {
-    setTodos(todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t));
+    if (!text.trim()) return;
+    const newTodo: Todo = { id: crypto.randomUUID(), text, completed: false };
+    setTodos([newTodo, ...todos]);
   };
-
-  const deleteTodo = (id: string) => {
-   setTodos(todos.filter((t) => t.id !== id))
+  const toggleTodo = (id: string) => {
+    setTodos(todos.map(t => t.id === id ? { ...t, completed: !t.completed } : t ))
   }
-
-
+  const deleteTodo = (id: string) => {
+    setTodos(todos.filter((t) => t.id !== id))
+  }
+  
   return (
     <div className='min-h-screen bg-gray-50 flex flex-col items-center py-10'>
       <h1 className='text-6xl sm: text-8xl font-black text-gray-200 opacity-50 mb-8 tracking-widset lowercase'> todos</h1>
@@ -37,6 +34,7 @@ function App() {
       </div>
     </div>
   );
+
 }
 
 export default App;
