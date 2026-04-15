21bdf6ef8634b3d23c76a5f2c5bcec853515cbad
mugisha thicien <mugishathicien04@gmail.com>
2026-04-15 14:11:43 +0200

    Working on how to apply useSearchParams with react router

---
+++ src/layout.js
@@ -0,0 +1,82 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route, Link, Outlet, useParams, useNavigate, useSearchParams } from 'react-router-dom';
+
+function App() {
+    return (
+        <BrowserRouter>
+            <nav>
+                <Link to='/' >Home</Link>
+                <Link to='/about' >About</Link>
+                <Link to='/contact'>Contact</Link>
+                <Link to='/services'>Services</Link>
+                <Link to='/courses'>Courses</Link>
+            </nav>
+            <Routes>
+                <Route path='/' element={<Home />} > Home page</Route> 
+                <Route path='/about' element={<About />} > About page</Route>
+                <Route path='/contact' element={<Contact />}>Contact page</Route>
+                <Route path='/services' element={<Services />}>
+                    <Route path='/web' element={<webServices />}></Route>
+                    <Route path='/mobile' element={<mobileServices />}></Route>
+                </Route>
+                <Route path='/courses/:id' element={<CoursesDetails />}>Courses page</Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+function Home() {
+    const navigation = useNavigate();
+    return <h1>Visit our home page</h1>
+}
+
+function About() {
+    return <h1>About page is here!</h1>
+}
+
+function Contact() {
+    return <h1>Contact us for more informations</h1>
+}
+
+function Services() {
+    return (
+        <>
+            <h1>Here's a list of our services</h1>  
+            <Link to='/web'>Web Services</Link>  
+            <Link to='/mobile'>Mobile Services</Link>
+            <Outlet/>
+        </>
+    )
+}
+
+function WebServices() {
+    return <h2>Access our services on website</h2>
+}
+
+function mobileServices() {
+    return <h2>Get access to our services on your mobile phone</h2>
+}
+
+function CoursesDetails() {
+    const { id } = useParams();
+    const [useSearchParams, setSearchParams] = useSearchParams();
+
+    const level = searchParams.get("level");
+    const price = searchParams.get("price");
+
+    return (
+        <>
+            <h1>Course ID: {id}</h1>
+            <h2>Level: {lever}</h2>
+            <h2>Price: {price}</h2>
+            <button onClick={() => setSearchParams({level: 'advanced', price: 'paid'})
+              }>upgrade course
+            </button>
+        </>
+        
+    )
+    return <h1>Get access to our online courses:{id }</h1>
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
