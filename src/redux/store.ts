17f6f8f0db3d5039595002dac3174ff158c36ef2
mugisha thicien <mugishathicien04@gmail.com>
2026-05-06 11:33:48 +0200

    Creating the interface to hold the todostates

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,55 +1,55 @@
-// import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { configureStore, createSlice, PayloadAction } from '@reduxjs/toolkit';
 
-// export interface Todo {
-//   id: string;
-//   text: string;
-//   completed: boolean;
-// }
+export interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
 
-// // interface TodoState {
-// //   todos: Todo[];
-// // }
+interface TodoState {
+  todos: Todo[];
+}
 
-// // const initialState: TodoState = {
-// //   todos: [],
-// // };
+const initialState: TodoState = {
+  todos: [],
+};
 
-// export const todoSlice = createSlice({
-//   name: 'todos',
-//   initialState,
-//   reducers: {
-//     addTodo: (state, action: PayloadAction<string>) => {
-//       state.todos.push({
-//         id: crypto.randomUUID(),
-//         text: action.payload,
-//         completed: false,
-//       });
-//     },
-//     editTodo: (state, action: PayloadAction<{ id: string; text: string }>) => {
-//       const todo = state.todos.find(t => t.id === action.payload.id);
-//       if (todo) {
-//         todo.text = action.payload.text;
-//       }
-//     },
-//     toggleTodo: (state, action: PayloadAction<string>) => {
-//       const todo = state.todos.find(t => t.id === action.payload);
-//       if (todo) {
-//         todo.completed = !todo.completed;
-//       }
-//     },
-//     removeTodo: (state, action: PayloadAction<string>) => {
-//       state.todos = state.todos.filter(t => t.id !== action.payload);
-//     },
-//   },
-// });
+export const todoSlice = createSlice({
+  name: 'todos',
+  initialState,
+  reducers: {
+    addTodo: (state, action: PayloadAction<string>) => {
+      state.todos.push({
+        id: crypto.randomUUID(),
+        text: action.payload,
+        completed: false,
+      });
+    },
+    editTodo: (state, action: PayloadAction<{ id: string; text: string }>) => {
+      const todo = state.todos.find(t => t.id === action.payload.id);
+      if (todo) {
+        todo.text = action.payload.text;
+      }
+    },
+    toggleTodo: (state, action: PayloadAction<string>) => {
+      const todo = state.todos.find(t => t.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+    removeTodo: (state, action: PayloadAction<string>) => {
+      state.todos = state.todos.filter(t => t.id !== action.payload);
+    },
+  },
+});
 
-// export const { addTodo, editTodo, toggleTodo, removeTodo } = todoSlice.actions;
+export const { addTodo, editTodo, toggleTodo, removeTodo } = todoSlice.actions;
 
-// export const store = configureStore({
-//   reducer: {
-//     todos: todoSlice.reducer,
-//   },
-// });
+export const store = configureStore({
+  reducer: {
+    todos: todoSlice.reducer,
+  },
+});
 
-// export type RootState = ReturnType<typeof store.getState>;
-// export type AppDispatch = typeof store.dispatch;
+export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
