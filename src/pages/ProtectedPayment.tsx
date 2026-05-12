6f2298ce6d8cc5d671a4d1012c0374929e30b84e
mugisha thicien <mugishathicien04@gmail.com>
2026-05-12 14:50:53 +0200

    Working on the impmementation of nested routes and how they should look like in the browser url

--- src/pages/ProtectedPayment.tsx
+++ src/pages/ProtectedPayment.tsx
@@ -9,7 +9,6 @@ type OutletcontextType = {
     cart: CartItem[];
     setCart: React.Dispatch<React.SetStateAction<CartItem[]>>;
 }
-
 export default function ProtectedPayment() {
     const { cart } = useOutletContext<OutletcontextType>();
     console.log(cart)
